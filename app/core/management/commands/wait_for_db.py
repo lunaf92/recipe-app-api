944065a03973a94deaf3816918528f43a5b364a2
Federico Lunardon <federico.lunardon@jrlgroup.co.uk>
2023-01-30 13:28:33 +0000

    fixed env variables

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,10 +21,9 @@ class Command(BaseCommand):
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write(
-                    self.style.WARNING(
                         "Database unavailable, waiting 1 second..."
-                    )
                 )
+                
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
