e339b8e4faad457300583d172088af9ecb634050
Federico Lunardon <federico.lunardon@jrlgroup.co.uk>
2023-01-30 10:14:52 +0000

    added function to wait for database to be ready

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^f_pr3^=+rz9qmy6do5cwf6+y11g@v1s)yyxq_db79b)#t54s&"
+SECRET_KEY = (
+    "django-insecure-^f_pr3^=+rz9qmy6do5cwf6+y11g@v1s)yyxq_db79b)#t54s&"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
